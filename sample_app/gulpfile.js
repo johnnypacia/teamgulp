b14d3a3e94fda03b09e116b8a5f28b43055d988b
Jess Lee <jessleenyc@gmail.com>
2016-01-19 14:26:46 -0500

    gulpfile

--- sample_app/gulpfile.js
+++ sample_app/gulpfile.js
@@ -3,7 +3,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minifyCSS = require('gulp-minify-css');
 var sass = require('gulp-sass');
+var install = require('gulp-install');
 
+
+//Runs NPM install 
+gulp.task('install', function() {
+  gulp.src(['./package.json'])
+  .pipe(install());
+});
 
 // CSS concat and minify
 gulp.task('styles', function() {
@@ -13,8 +20,27 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('./build/styles/'));
 });
 
+//SASS to CSS compiler
 gulp.task('sass', function () {
   gulp.src('./src/styles/*.scss')
    .pipe(sass({outputStyle: 'compressed'}))
    .pipe(gulp.dest('./build/styles/'));
 });
+
+
+//Runs all task with one command
+gulp.task('default', ['sass','styles'], function() {
+  console.log('defaultin')
+
+  gulp.watch('./src/styles/*.scss', function() {
+    console.log('sassin')
+    gulp.run('sass');
+  })
+
+  gulp.watch('./src/styles/*.css'), function() {
+    gulp.run('styles');
+  }
+
+});
+
+
